e6c66ad53f83c833ad5609c25395b277727df6a8
Pierre Martin <pierre@occi-tech.com>
2013-01-21 11:26:58 +0100

    Added support for Composer based deployments for CakePHP apps
    Squashed commit of the following:
    
    commit 5cdf4e473e168e6ee2a6b872922dff68ef3add36
    Author: Pierre Martin <pierre@occi-tech.com>
    Date:   Mon Jan 21 11:25:29 2013 +0100
    
        Fixed the CakePHP binary path update to use relative path and updated migrations command
    
    commit ac71d368cb9e7d19afc5b5953ab53112b1f2ba0a
    Author: Pierre Martin <pierre@occi-tech.com>
    Date:   Fri Jan 18 15:44:49 2013 +0100
    
        Initial version of a Composer deployment for CakePHP2.x

--- CakePHP2x-Recipe.rb
+++ CakePHP2x-Recipe.rb
@@ -14,7 +14,12 @@ _cset(:app_shared_files) { ["/Config/database.php"] }
 _cset(:cake_repo) { "git://github.com/cakephp/cakephp.git" }
 _cset(:cake_branch) { "master" }
 _cset(:cake_migrations) { ['app'] }
+_cset(:cake_binary) { "/cake/lib/Cake/Console/cake" }
 _cset(:app_path) { "" } # path of your "app" directory from the base of the repository without trailing slash
+
+_cset(:php_bin) { "php" }
+_cset(:composer_bin) { false }
+_cset(:composer_options) { "--no-scripts --verbose --prefer-dist" }
 
 # Used by the interactive database creation prompt
 def defaults(val, default)
@@ -24,25 +29,13 @@ end
 
 #########
 # Based on the following resources
-#		- https://github.com/cakephp/cakepackages/blob/master/Config/deploy.rb
-#		- http://mark-story.com/posts/view/deploying-a-cakephp-site-with-capistrano
+# 	- https://github.com/cakephp/cakepackages/blob/master/Config/deploy.rb
+# 	- http://mark-story.com/posts/view/deploying-a-cakephp-site-with-capistrano
 #   - https://github.com/jadb/capcake/blob/master/lib/capcake.rb
 #   - http://www.assembla.com/code/cakephp_svn/subversion/nodes/trunk/deploy/deploy.rb?affiliate=d1rk
 namespace :cake do
 
-	desc <<-DESC
-			Prepares one or more servers for deployment of CakePHP.
-
-			By default, it will create a shallow clone of the CakePHP repository
-			inside shared_path/cake and run cake:update.
-			It will also create needed directories
-
-			It is safe to run this task on servers that have already been set up; it
-			will not destroy any deployed revisions or data.
-	DESC
 	task :setup, :roles => :web, :except => { :no_release => true } do
-		run "cd #{shared_path} && rm -rf cake && git clone --depth 1 #{cake_repo} cake"
-
 		if app_shared_dirs
 				app_shared_dirs.each { |link| run "#{try_sudo} mkdir -p #{shared_path}#{link} && chmod 777 #{shared_path}#{link}"}
 		end
@@ -59,9 +52,9 @@ namespace :cake do
 	end
 
 	desc 'Generate / Replace the database.php file'
-  task :create_db_config do
+	task :create_db_config do
 		require 'erb'
-    on_rollback { run "rm #{shared_path}/Config/database.php" }
+		on_rollback { run "rm #{shared_path}/Config/database.php" }
 
 		puts "Database configuration"
 		_cset :db_driver, defaults(Capistrano::CLI.ui.ask("driver [Database/Mysql]:"), 'Database/Mysql')
@@ -73,7 +66,7 @@ namespace :cake do
 		_cset :db_persistent, defaults(Capistrano::CLI.ui.ask("persistent [false]:"), 'false')
 		_cset :db_encoding, defaults(Capistrano::CLI.ui.ask("encoding [utf8]:"), 'utf8')
 
-    DATABASE_CONFIG_TPL = <<-TEXT
+		DATABASE_CONFIG_TPL = <<-TEXT
 <?php
 class DATABASE_CONFIG {
 
@@ -91,11 +84,12 @@ class DATABASE_CONFIG {
 ?>
 TEXT
 		DATABASE_CONFIG = ERB.new(DATABASE_CONFIG_TPL).result(binding)
-    put DATABASE_CONFIG, "#{shared_path}/Config/database.php", :mode => 0644
-  end
+		put DATABASE_CONFIG, "#{shared_path}/Config/database.php", :mode => 0644
+	end
 
 	desc <<-DESC
 		Force CakePHP installation to checkout a new branch/tag
+		If no remote cache exists the repository will be cloned
 
 		By default, it will checkout the :cake_branch you set in
 		deploy.rb, but you can change that on runtime by specifying
@@ -106,8 +100,11 @@ TEXT
 	DESC
 	task :update do
 		set :cake_branch, ENV['BRANCH'] if ENV.has_key?('BRANCH')
-		stream "cd #{shared_path}/cake && git fetch && git checkout #{cake_branch} && git pull"
 
+		# Clone if the repository does not exists
+		run "if [[ ! -d #{shared_path}/cake ]]; then cd #{shared_path} && rm -rf cake && git clone --depth 1 #{cake_repo} cake; fi"
+
+		stream "cd #{shared_path}/cake && git fetch && git checkout #{cake_branch} && git pull"
 		run "#{try_sudo} rm -rf #{releases_path}/lib"
 	end
 
@@ -131,7 +128,7 @@ TEXT
 			end
 
 			sed_command = "sed -i '2idefine(\"CAKE_CORE_INCLUDE_PATH\",  \"#{shared_path}/cake/lib\");' #{latest_release}#{app_path}/webroot/index.php"
-			run sed_command
+			run "if [[ -d #{shared_path}/cake ]]; then #{sed_command}; fi"
 	end
 
 	desc 'Blow up all the cache files CakePHP uses, ensuring a clean restart.'
@@ -153,8 +150,7 @@ TEXT
 	desc 'Chmod Cake directories'
 	task :chmod do
 		run [
-			"chmod -R 777 #{shared_path}/tmp",
-			# Add here upload directories and other app-specif files
+			"chmod -R 777 #{shared_path}/tmp"
 		].join(' && ')
 	end
 
@@ -184,7 +180,7 @@ TEXT
 	DESC
 	task :shell do
 		command = variables[:command] || ""
-		run "#{shared_path}/cake/lib/Cake/Console/cake -app #{latest_release}#{app_path} #{command}"
+		run "#{deploy_to}/#{current_dir}#{cake_binary} -app #{latest_release}#{app_path} #{command}"
 	end
 
 	## Tasks involving migrations
@@ -194,23 +190,60 @@ TEXT
 			run "cd #{deploy_to}/#{current_dir}"
 			cake_migrations.each do |plugin|
 				if plugin == "app"
-					run "#{shared_path}/cake/lib/Cake/Console/cake -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration all"
+					run "#{deploy_to}/#{current_dir}#{cake_binary} -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration run all"
 				else
-					run "#{shared_path}/cake/lib/Cake/Console/cake -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration all --plugin #{plugin}"
+					run "#{deploy_to}/#{current_dir}#{cake_binary} -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration run all --plugin #{plugin}"
 				end
 			end
 		end
 
 		desc 'Gets the status of CakeDC Migrations'
 		task :status, :roles => :db, :only => { :primary => true } do
-			run "cd #{deploy_to}/#{current_dir} && #{shared_path}/cake/lib/Cake/Console/cake -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration status"
+			run "cd #{deploy_to}/#{current_dir} && #{deploy_to}/#{current_dir}#{cake_binary} -app #{deploy_to}/#{current_dir}#{app_path} Migrations.migration status"
 		end
 
 		# TODO Implement a revert method
 	end
 
+	## Composer related installation
+	# inspired from capyfony / symfony.composer (@see https://github.com/everzet/capifony/blob/master/lib/symfony2/symfony.rb)
+	# TODO Extracts "'true' == capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip" in a remote_file_exists? method
+	namespace :composer do
+
+		desc "Ensure the latest Composer version is available - Gets composer and installs it or just update"
+		task :get, :roles => :app, :except => { :no_release => true } do
+			if 'true' == capture("if [ -e #{previous_release}/composer.phar ]; then echo 'true'; fi").strip
+				run "#{try_sudo} sh -c 'cp #{previous_release}/composer.phar #{latest_release}/'"
+			end
+
+			if 'true' == capture("if [ -e #{latest_release}/composer.phar ]; then echo 'true'; fi").strip
+				run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} composer.phar self-update'"
+			else
+				run "#{try_sudo} sh -c 'cd #{latest_release} && curl -s http://getcomposer.org/installer | #{php_bin}'"
+			end
+		end
+
+		desc "Runs composer to install vendors from composer.lock file"
+		task :install, :roles => :app, :except => { :no_release => true } do
+			if !composer_bin
+				cake.composer.get
+				set :composer_bin, "#{php_bin} composer.phar"
+			end
+
+			run "#{try_sudo} sh -c 'cd #{latest_release} && #{composer_bin} install #{composer_options}'"
+		end
+
+		desc "Copying vendors from previous release"
+		task :copy_vendors, :except => { :no_release => true } do
+			run "vendorDir=#{current_path}/Vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir #{latest_release}/Vendor; fi;"
+		end
+	end
+
 end
 
 after   'deploy:setup', 'cake:setup'
-before  'deploy:finalize_update', 'cake:update'
 after   'deploy:finalize_update', 'cake:finalize_update'
+
+# Uncomment or add this in your deploy.rb to use a Git install of CakePHP
+# leave this line commented if you manage your Cake installation yourself (using composer for instance)
+# before  'deploy:finalize_update', 'cake:update'
