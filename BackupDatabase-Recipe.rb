aa99518cfd2d2408954a30e90228d45b3cc893e4
Damien Biasotto <damien@occi-tech.com>
2015-01-09 16:36:31 +0100

    Use unless block to check database credentials and raise error.

--- BackupDatabase-Recipe.rb
+++ BackupDatabase-Recipe.rb
@@ -8,32 +8,32 @@ namespace :database do
     file = "/tmp/#{filename}"
     on_rollback { delete file }
 
+    unless exists?(:db_credentials)
+      raise("Cannot access database for #{application}")
+    end
 
     logger.info("Dumping database")
-    if db_credentials.length > 0
-      run "mysqldump  --add-drop-table --extended-insert --force -u #{db_credentials["username"]} --password=#{db_credentials["password"]} #{db_credentials["dbname"]} -h #{db_credentials["host"]} | gzip > #{file}"  do |ch, stream, data|
-        puts data
-      end
-
-      get file, "#{current_release}#{app_path}/#{filename}"
-      File.delete(file)
-    else
-      logger.important("Unsupported type of project - No backup will be provided")
+    run "mysqldump  --add-drop-table --extended-insert --force -u #{db_credentials["username"]} --password=#{db_credentials["password"]} #{db_credentials["dbname"]} -h #{db_credentials["host"]} | gzip > #{file}"  do |ch, stream, data|
+      puts data
     end
+
+    get file, "#{current_release}#{app_path}/#{filename}"
+    File.delete(file)
   end
 
   desc <<-DESC
 		Revert a database thanks to database backup
   DESC
   task :revert do
-
     database_dump = "#{previous_release}#{app_path}/#{application}.dump.sql.gz"
 
-    if File.exist?(database_dump)
-      run "zcat #{database_dump} | mysql -u #{db_credentials["username"]} --password=#{db_credentials["password"]} #{db_credentials["dbname"]} -h #{db_credentials["host"]}"
-    else
-      logger.important("No database backup found")
+    unless File.exist?(database_dump)
+      raise("No database backup found")
     end
+    unless exists?(:db_credentials)
+      raise("Cannot access database for #{application}")
+    end
+    run "zcat #{database_dump} | mysql -u #{db_credentials["username"]} --password=#{db_credentials["password"]} #{db_credentials["dbname"]} -h #{db_credentials["host"]}"
   end
 
 end
